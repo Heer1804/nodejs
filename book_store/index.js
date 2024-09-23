32da3e80c41df3b780883a8c0f83effbe361b279
Heer1804 <parikhheer8@gmail.com>
2024-09-23 18:18:54 +0530

    c

---
+++ book_store/index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const db = require('./config/database.js');
+const bodyParser = require('body-parser');
+const userModel = require('./models/userSchema');
+
+const port = 8086;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("node_modules"));
+// app.use(express.static("public"))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    userModel.find({}).then((data) => {
+        return res.render('index', {
+            data
+        });
+    }).catch((err) => {
+        console.log(err);
+        return false;
+    })
+})
+
+app.post('/insertData', (req, res) => {
+
+    let editId = req.body.editId;
+
+    if (editId) {
+        userModel.findByIdAndUpdate(editId, { ...req.body }).then((data) => {
+            console.log("data updated.");
+            return res.redirect('/');
+        }).catch((err) => {
+            console.log(err);
+            return false;
+        })
+    }
+    else {
+        userModel.create({ ...req.body }).then((data) => {
+            return res.redirect('/');
+        }).catch((err) => {
+            console.log(err);
+            return false;
+        })
+    }
+
+})
+
+app.get('/deleteData/:id', (req, res) => {
+    let { id } = req.params;
+
+    userModel.findByIdAndDelete(id).then((data) => {
+        console.log("Data deleted..");
+        return res.redirect('/');
+    }).catch((err) => {
+        console.log(err);
+        return false;
+    })
+
+})
+
+app.get('/editData/:id', (req, res) => {
+    let { id } = req.params;
+    userModel.findById(id).then((data) => {
+        console.log(data);
+
+        return res.render('edit', {
+            data
+        });
+    }).catch((err) => {
+        console.log(err);
+        return false;
+    })
+
+
+})
+
+app.listen(port, (err) => {
+    if (!err) {
+        db
+        console.log("server Start \nhttp://localhost:" + port);
+    }
+})
---
+++ book_store/models/userSchema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    pages: {
+        type: String,
+        required: true
+    },
+    language: {
+        type: Array,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+})
+
+const userModel = mongoose.model('userTbl', userSchema);
+
+module.exports = userModel;
