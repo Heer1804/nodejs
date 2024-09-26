0c1f7eb0469a7c911f7592d537d80032b9c0f152
Heer1804 <parikhheer8@gmail.com>
2024-09-26 17:44:13 +0530

    c

--- book_store/models/userSchema.js
+++ book_store/models/userSchema.js
@@ -24,6 +24,10 @@ const userSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true
+    },
+    city: {
+        type: String,
+        required: true
     }
 })
 
